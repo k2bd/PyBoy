d2be5f7f145534231e58ce2d21a9bcaf817c7334
Kevin Duff <kevinkelduff@gmail.com>
2022-04-03 17:23:05 +0100

    Update temp RAM addresses for GSC

--- pyboy/__main__.py
+++ pyboy/__main__.py
@@ -21,7 +21,9 @@ INTERNAL_LOADSTATE = "INTERNAL_LOADSTATE_TOKEN"
 
 def color_tuple(string):
     color_palette = [int(c.strip(), 16) for c in string.split(",")]
-    assert len(color_palette) == 4, f"Not the correct amount of colors! Expected four, got {len(color_palette)}"
+    assert (
+        len(color_palette) == 4
+    ), f"Not the correct amount of colors! Expected four, got {len(color_palette)}"
     return color_palette
 
 
@@ -36,22 +38,32 @@ parser = argparse.ArgumentParser(
     description="PyBoy -- Game Boy emulator written in Python",
     epilog="Warning: Features marked with (internal use) might be subject to change.",
 )
-parser.add_argument("ROM", type=valid_file_path, help="Path to a Game Boy compatible ROM file")
-parser.add_argument("-b", "--bootrom", type=valid_file_path, help="Path to a boot-ROM file")
-parser.add_argument("--profiling", action="store_true", help="Enable opcode profiling (internal use)")
-parser.add_argument("--randomize-ram", action="store_true", help="Randomize Game Boy RAM on startup")
+parser.add_argument(
+    "ROM", type=valid_file_path, help="Path to a Game Boy compatible ROM file"
+)
+parser.add_argument(
+    "-b", "--bootrom", type=valid_file_path, help="Path to a boot-ROM file"
+)
+parser.add_argument(
+    "--profiling", action="store_true", help="Enable opcode profiling (internal use)"
+)
+parser.add_argument(
+    "--randomize-ram", action="store_true", help="Randomize Game Boy RAM on startup"
+)
 parser.add_argument(
     "--log-level",
     default="INFO",
     type=str,
     choices=["ERROR", "WARNING", "INFO", "DEBUG", "DISABLE"],
-    help="Set logging level"
+    help="Set logging level",
 )
 parser.add_argument(
     "--color-palette",
     type=color_tuple,
     default=defaults["color_palette"],
-    help=('Four comma seperated, hexadecimal, RGB values for colors (i.e. "FFFFFF,999999,555555,000000")')
+    help=(
+        'Four comma seperated, hexadecimal, RGB values for colors (i.e. "FFFFFF,999999,555555,000000")'
+    ),
 )
 parser.add_argument(
     "-l",
@@ -63,7 +75,7 @@ parser.add_argument(
     help=(
         "Load state from file. If filepath is specified, it will load the given path. Otherwise, it will automatically "
         "locate a saved state next to the ROM file."
-    )
+    ),
 )
 parser.add_argument(
     "-w",
@@ -72,10 +84,20 @@ parser.add_argument(
     default=defaults["window_type"],
     type=str,
     choices=["SDL2", "OpenGL", "headless", "dummy"],
-    help="Specify window-type to use"
+    help="Specify window-type to use",
 )
-parser.add_argument("-s", "--scale", default=defaults["scale"], type=int, help="The scaling multiplier for the window")
-parser.add_argument("--disable-renderer", action="store_true", help="Disables screen rendering for higher performance")
+parser.add_argument(
+    "-s",
+    "--scale",
+    default=defaults["scale"],
+    type=int,
+    help="The scaling multiplier for the window",
+)
+parser.add_argument(
+    "--disable-renderer",
+    action="store_true",
+    help="Disables screen rendering for higher performance",
+)
 parser.add_argument("--sound", action="store_true", help="Enable sound (beta)")
 
 for arguments in parser_arguments():
@@ -145,6 +167,13 @@ See "pyboy --help" for how to enable rewind and other awesome features!
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect(("127.0.0.1", 55722))
 
+        # Temp - limit listening range to just relevant ranges for testing.
+        to_listen = (
+            list(range(0xD2F7, 0xD31D))
+            + list(range(0xDBE4, 0xDC24))
+            + list(range(0xDE99, 0xDED9))
+        )
+
         while not pyboy.tick():
             frame += 1
 
@@ -154,7 +183,7 @@ See "pyboy --help" for how to enable rewind and other awesome features!
                         "location": addr,
                         "value": pyboy.get_memory_value(addr),
                     }
-                    for addr in range(0xD2F7, 0xD31D)  # Temp - pokemon seen/owned in RED
+                    for addr in to_listen
                 ]
                 ram_json = {
                     "frame": frame,
@@ -174,8 +203,11 @@ See "pyboy --help" for how to enable rewind and other awesome features!
 def profiling_printer(hitrate):
     print("Profiling report:")
     from operator import itemgetter
+
     names = [core.opcodes.CPU_COMMANDS[n] for n in range(0x200)]
-    for hits, opcode, name in sorted(filter(itemgetter(0), zip(hitrate, range(0x200), names)), reverse=True):
+    for hits, opcode, name in sorted(
+        filter(itemgetter(0), zip(hitrate, range(0x200), names)), reverse=True
+    ):
         yield ("%3x %16s %s" % (opcode, name, hits))
 
 
